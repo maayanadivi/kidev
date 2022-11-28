26163503a5405d0fe2ddbe61bb46573d1df2cd15
alice-aidlin <aliceai@ac.sce.ac.il>
2022-11-28 11:03:45 +0200

    Update App.js
    commint pages

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Learn from "./components/pages/Learn";
 import SignUp from "./components/pages/SignUp";
-import Unit1 from "./components/pages/Unit1";
-import Unit2 from "./components/pages/Unit2";
-import Unit3 from "./components/pages/Unit3";
-import Soon from "./components/pages/Soon";
+// import Unit1 from "./components/pages/Unit1";
+// import Unit2 from "./components/pages/Unit2";
+// import Unit3 from "./components/pages/Unit3";
+// import Soon from "./components/pages/Soon";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/services" element={Services} />
-          <Route path="/learn" element={Learn} />
-          <Route path="/unit1" exact element={<Unit1 />} />
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" exact element={<Home />} />
+					<Route path="/services" element={Services} />
+					<Route path="/learn" element={Learn} />
+					{/* <Route path="/unit1" exact element={<Unit1 />} />
           <Route path="/unit2" exact element={<Unit2 />} />
           <Route path="/unit3" exact element={<Unit3 />} />
           <Route path="/soon" exact element={<Soon />} />
-          <Route path="/soon" exact element={<Soon />} />
-          <Route path="/sign-up" element={SignUp} />
-        </Routes>
-      </Router>
-    </>
-  );
+          <Route path="/soon" exact element={<Soon />} /> */}
+					<Route path="/sign-up" element={SignUp} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
