de74d46339b5d3870c3cd8803d061b3f03bebcbf
maayanadivi <maayana2@ac.sce.ac.il>
2022-12-12 22:50:27 +0200

    cards-mamoryGame

--- src/components/games/memoryGame/Game.js
+++ src/components/games/memoryGame/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../games/memoryGame/Game.css";
 import ring from "./imgGame/ring-1.png";
 import helmet from "./imgGame/helmet-1.png";
@@ -6,50 +6,71 @@ import potion from "./imgGame/potion-1.png";
 import scroll from "./imgGame/scroll-1.png";
 import shield from "./imgGame/shield-1.png";
 import sword from "./imgGame/sword-1.png";
-import cover from "./imgGame/cover.png";
+//import cover from "./imgGame/cover.png";
+import SingleCard from "./SingleCard";
 
 const cardImages = [
-	{ src: ring },
-	{ src: sword },
-	{ src: shield },
-	{ src: potion },
-	{ src: helmet },
-	{ src: scroll },
+  { src: ring },
+  { src: sword },
+  { src: shield },
+  { src: potion },
+  { src: helmet },
+  { src: scroll },
 ];
 
 function Game() {
-	const [cards, setCards] = useState([]);
-	const [turns, setTurns] = useState(0);
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
-	// shuffle cards
-	const shuffleCards = () => {
-		const shuffledCards = [...cardImages, ...cardImages]
-			.sort(() => Math.random() - 0.5)
-			.map((card) => ({ ...card, id: Math.random() }));
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
 
-		setCards(shuffledCards);
-		setTurns(0); // resat the turns
-	};
+    setCards(shuffledCards);
+    setTurns(0); // resat the turns
+  };
 
-	console.log(cards, turns);
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
-	return (
-		<div className="Game">
-			<h1> Magic Match </h1>
-			<button onClick={shuffleCards}>New Game</button>
+  //compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("cards match");
+        resetTurn();
+      } else {
+        console.log("card not match");
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
 
-			<div className="card-grid">
-				{cards.map((card) => (
-					<div className="card" key={card.id}>
-						<div>
-							<img className="front" src={card.src} alt="card front" />
-							<img className="back" src={cover} alt="card back" />
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  //reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
+  return (
+    <div className="Game">
+      <h1> Magic Match </h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Game;
---
+++ src/components/games/memoryGame/SingleCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./singleCard.css";
+import cover from "./imgGame/cover.png";
+
+export default function SingleCard({ card, handleChoice }) {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src={cover}
+          onClick={handleClick}
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
+}
