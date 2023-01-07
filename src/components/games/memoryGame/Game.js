e864a6c4ac6312a025eca6fe7620c00fb1068fdd
alice-aidlin <aliceai@ac.sce.ac.il>
2023-01-07 19:37:19 +0200

    added content to unit 1

--- src/components/games/memoryGame/Game.js
+++ src/components/games/memoryGame/Game.js
@@ -10,91 +10,93 @@ import sword from "./imgGame/6.png";
 import SingleCard from "./SingleCard";
 
 const cardImages = [
-  { src: ring, matched: false },
-  { src: sword, matched: false },
-  { src: shield, matched: false },
-  { src: potion, matched: false },
-  { src: helmet, matched: false },
-  { src: scroll, matched: false },
+	{ src: ring, matched: false },
+	{ src: sword, matched: false },
+	{ src: shield, matched: false },
+	{ src: potion, matched: false },
+	{ src: helmet, matched: false },
+	{ src: scroll, matched: false },
 ];
 
 function Game() {
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
-  const [disabled, setDisabled] = useState(false);
+	const [cards, setCards] = useState([]);
+	const [turns, setTurns] = useState(0);
+	const [choiceOne, setChoiceOne] = useState(null);
+	const [choiceTwo, setChoiceTwo] = useState(null);
+	const [disabled, setDisabled] = useState(false);
 
-  // shuffle cards
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setCards(shuffledCards);
-    setTurns(0); // resat the turns
-  };
+	// shuffle cards
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }));
+		setChoiceOne(null);
+		setChoiceTwo(null);
+		setCards(shuffledCards);
+		setTurns(0); // resat the turns
+	};
 
-  // handle a choice
-  const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-  };
+	// handle a choice
+	const handleChoice = (card) => {
+		choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+	};
 
-  //compare 2 selected cards
-  useEffect(() => {
-    if (choiceOne && choiceTwo) {
-      setDisabled(true);
-      if (choiceOne.src === choiceTwo.src) {
-        setCards((prevCards) => {
-          return prevCards.map((card) => {
-            if (card.src === choiceOne.src) {
-              return { ...card, matched: true };
-            } else {
-              return card;
-            }
-          });
-        });
-        resetTurn();
-      } else {
-        setTimeout(() => resetTurn(), 1000);
-      }
-    }
-  }, [choiceOne, choiceTwo]);
-  console.log(cards);
+	//compare 2 selected cards
+	useEffect(() => {
+		if (choiceOne && choiceTwo) {
+			setDisabled(true);
+			if (choiceOne.src === choiceTwo.src) {
+				setCards((prevCards) => {
+					return prevCards.map((card) => {
+						if (card.src === choiceOne.src) {
+							return { ...card, matched: true };
+						} else {
+							return card;
+						}
+					});
+				});
+				resetTurn();
+			} else {
+				setTimeout(() => resetTurn(), 1000);
+			}
+		}
+	}, [choiceOne, choiceTwo]);
+	console.log(cards);
 
-  //reset choices & increase turn
-  const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setTurns((prevTurns) => prevTurns + 1);
-    setDisabled(false);
-  };
+	//reset choices & increase turn
+	const resetTurn = () => {
+		setChoiceOne(null);
+		setChoiceTwo(null);
+		setTurns((prevTurns) => prevTurns + 1);
+		setDisabled(false);
+	};
 
-  // start a new game automaticly
-  useEffect(() => {
-    shuffleCards();
-  }, []);
+	// start a new game automaticly
+	useEffect(() => {
+		shuffleCards();
+	}, []);
 
-  return (
-    <div className="Game">
-      <h1> Magic Match </h1>
-      <button onClick={shuffleCards}>New Game</button>
+	return (
+		<div className="Game">
+			<h1> Magic Match </h1>
+			<button style={{ marginTop: "20px" }} onClick={shuffleCards}>
+				New Game
+			</button>
 
-      <div className="card-grid">
-        {cards.map((card) => (
-          <SingleCard
-            key={card.id}
-            card={card}
-            handleChoice={handleChoice}
-            flipped={card === choiceOne || card === choiceTwo || card.matched}
-            disabled={disabled}
-          />
-        ))}
-      </div>
-      <p>Turns: {turns}</p>
-    </div>
-  );
+			<div className="card-grid">
+				{cards.map((card) => (
+					<SingleCard
+						key={card.id}
+						card={card}
+						handleChoice={handleChoice}
+						flipped={card === choiceOne || card === choiceTwo || card.matched}
+						disabled={disabled}
+					/>
+				))}
+			</div>
+			<p style={{ marginTop: "20px" }}>Turns: {turns}</p>
+		</div>
+	);
 }
 
 export default Game;
--- src/components/pages/Unit1.js
+++ src/components/pages/Unit1.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "../../App.css";
-import Footer from "../Footer";
+import a1 from "../../assets/unit1/a1.png";
+import a2 from "../../assets/unit1/a2.png";
+import a3 from "../../assets/unit1/a3.png";
+import a4 from "../../assets/unit1/a4.png";
+
 import Game from "../games/memoryGame/Game";
+import Footer from "../Footer";
 
 function Unit1() {
-  return (
-    <>
-      <Game />
+	return (
+		<>
+			<img src={a1} alt={""} />
+			<img src={a2} alt={""} />
+			<img src={a3} alt={""} />
+			<img src={a4} alt={""} />
 
-      <Footer />
-    </>
-  );
+			<Game />
+
+			<Footer />
+		</>
+	);
 }
 
 export default Unit1;
