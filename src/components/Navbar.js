c1a0103a61a4f45910dfcd6aea3a0a1f9ecd77ba
maayanadivi <maayana2@ac.sce.ac.il>
2023-04-28 17:30:42 +0300

    add unit 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,70 +2,76 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 const scrollToTop = () => {
-	window.scrollTo({
-		top: 0,
-		left: 100,
-		behavior: "smooth",
-	});
+  window.scrollTo({
+    top: 0,
+    left: 100,
+    behavior: "smooth",
+  });
 };
 
 const getStartScrollTo = () => {
-	window.scrollTo({
-		top: 750,
-		left: 100,
-		behavior: "smooth",
-	});
+  window.scrollTo({
+    top: 750,
+    left: 100,
+    behavior: "smooth",
+  });
+};
+
+const scrollToBottom = () => {
+  const pageHeight = document.documentElement.scrollHeight;
+  const viewPortHeight = document.documentElement.clientHeight;
+
+  window.scrollTo({
+    top: pageHeight - viewPortHeight,
+    behavior: "smooth",
+  });
 };
 
 export const Navbar = () => {
-	const [click, setClick] = useState(false);
-	const [button, setButton] = useState(true);
-	const handleClick = () => setClick(!click);
-	const closeMobileMenu = () => setClick(false);
-	const showButton = () => {
-		if (window.innerWidth <= 960) {
-			setButton(false);
-		} else {
-			setButton(true);
-		}
-	};
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-	useEffect(() => {
-		showButton();
-	}, [button]);
+  useEffect(() => {
+    showButton();
+  }, [button]);
 
-	window.addEventListener("resize", showButton);
-	return (
-		<>
-			<nav className="navbar">
-				<div className="navbar-container">
-					<Link to="/" className="navbar-logo" onClick={scrollToTop}>
-						KIDEV <i class="fa-solid fa-chalkboard"></i>
-					</Link>
-					<div className="menu-icon" onClick={handleClick}>
-						<i className={click ? "fas fa-bars" : "fas fa-bars"} />
-					</div>
-					<ul className={click ? "nam-menu active" : "nav-menu"}>
-						<li className="nav-item">
-							<Link to="/" className="nav-links" onClick={scrollToTop}>
-								Home
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link
-								to="/Services"
-								className="nav-links"
-								onClick={closeMobileMenu}
-							>
-								Services
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-links" onClick={getStartScrollTo}>
-								Learn
-							</Link>
-						</li>
-						{/* <li className="nav-item">
+  window.addEventListener("resize", showButton);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={scrollToTop}>
+            KIDEV <i class="fa-solid fa-chalkboard"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-bars" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nam-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={scrollToTop}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-links" onClick={scrollToBottom}>
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={getStartScrollTo}>
+                Learn
+              </Link>
+            </li>
+            {/* <li className="nav-item">
               <Link
                 to="/sign-up"
                 className="nav-links"
@@ -74,12 +80,12 @@ export const Navbar = () => {
                 Sign-up
               </Link>
             </li> */}
-					</ul>
-					{/* {button && <Button buttonStyle="btn--outline">SING UP</Button>} */}
-				</div>
-			</nav>
-		</>
-	);
+          </ul>
+          {/* {button && <Button buttonStyle="btn--outline">SING UP</Button>} */}
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
--- src/components/pages/Unit2.js
+++ src/components/pages/Unit2.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "../../App.css";
+import b1 from "../../assets/unit2/b1.png";
+import b2 from "../../assets/unit2/b2.png";
+import b3 from "../../assets/unit2/b3.png";
+import b4 from "../../assets/unit2/b4.png";
+
 import Footer from "../Footer";
+import QuizGame from "../games/TriviaGame/QuizGame";
 
 function Unit2() {
   return (
     <>
-      <h1> UNIT 2 - INPUT AND CHARACTERS </h1>
+      <img src={b1} alt={""} />
+      <img src={b2} alt={""} />
+      <img src={b3} alt={""} />
+      <img src={b4} alt={""} />
+      <QuizGame />
+
       <Footer />
     </>
   );
